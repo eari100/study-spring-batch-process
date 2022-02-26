ae5d23337d5026174073c339cdb53d11c8f30ec2
Jaewook Choi <eari100@naver.com>
2022-02-26 21:25:52 +0900

    Add Business Class

---
+++ src/main/java/com/example/studyspringbatchprocess/Person.java
@@ -0,0 +1,13 @@
+package com.example.studyspringbatchprocess;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Person {
+    private String lastName;
+    private String firstName;
+}
