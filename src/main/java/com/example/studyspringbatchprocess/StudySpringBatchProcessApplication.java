4ab6a55727a65283d5bf05a97aaf334c373971df
Jaewook Choi <eari100@naver.com>
2022-02-26 21:16:44 +0900

    Initial commit

---
+++ src/main/java/com/example/studyspringbatchprocess/StudySpringBatchProcessApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringbatchprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBatchProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBatchProcessApplication.class, args);
+    }
+
+}
