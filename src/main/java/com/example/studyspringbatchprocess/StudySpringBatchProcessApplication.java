831e639845278338ad965da51c5bba736a6726eb
Jaewook Choi <eari100@naver.com>
2022-02-26 22:08:34 +0900

    Add executable to application

--- src/main/java/com/example/studyspringbatchprocess/StudySpringBatchProcessApplication.java
+++ src/main/java/com/example/studyspringbatchprocess/StudySpringBatchProcessApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudySpringBatchProcessApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(StudySpringBatchProcessApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(StudySpringBatchProcessApplication.class, args)));
     }
-
 }
